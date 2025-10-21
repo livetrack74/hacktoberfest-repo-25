1dcdb30f8111d88b76c1b3766ec2ce23129a30e1
shouvik-maity <shouvikmaity1234@gmail.com>
2025-10-22 00:23:56 +0530

    Pattern addition

---
+++ patterns/cpp/floydstriangle.cpp
@@ -0,0 +1,16 @@
+// 1
+// 21
+// 321
+// 4321
+#include <iostream>
+using namespace std;
+int main() {
+    int n=4;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+ return 0;
+}
