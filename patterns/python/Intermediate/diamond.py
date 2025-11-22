3371627eb9f97af3c72fb974ef71285520a80294
Nexalix790 <nexalix.dev@gmail.com>
2025-11-22 09:30:23 +0100

    Implement input validation for diamond pattern rows
    Added input validation for the number of rows.

--- patterns/python/Intermediate/diamond.py
+++ patterns/python/Intermediate/diamond.py
@@ -1,4 +1,15 @@
-rows = int(input("Enter the number of rows: "))
+
+# Gestion of invalid input
+
+while True:   
+    try:
+        rows = int(input("Enter the number of rows: ")) # Imput from user
+        break                                           # Exit loop if input is valid   
+    except ValueError:                                  # Handle invalid input   
+        print("Invalid input. Please enter a number.")  # Prompt user again
+    
+
+# ------ Diamond Pattern ------ #
 for i in range(1, rows+1):
     print((rows-i)*" ", end="")
     print(i*"*", end="")
@@ -7,3 +18,4 @@ for j in range(rows-1, 0, -1):
     print((rows-j)*" ", end="")
     print(j*"*", end="")
     print((j-1)*"*")
+# ------ Diamond Pattern ------ #
